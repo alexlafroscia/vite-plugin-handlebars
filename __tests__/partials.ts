d22ef98e75a44739758422567fdbb5f57c55262b
Alex LaFroscia <alex@lafroscia.com>
2021-02-03 16:47:40 -0500

    fix: handle empty partials directory
    Closes #1

--- __tests__/partials.ts
+++ __tests__/partials.ts
@@ -1,11 +1,13 @@
 import { resolve } from 'path';
+import { mkdtemp } from 'fs/promises';
 import { build, fixtureFor, getHtmlSource } from './helpers';
 
-const APP_PATH = fixtureFor('partials');
+const BASIC_PARTIAL_APP = fixtureFor('partials');
+const NO_PARTIAL_DIRECTORY_APP = fixtureFor('no-partial-directory');
 
 test('it allows for injecting a partial', async () => {
-  const result = await build(APP_PATH, {
-    partialDirectory: resolve(APP_PATH, 'partials'),
+  const result = await build(BASIC_PARTIAL_APP, {
+    partialDirectory: resolve(BASIC_PARTIAL_APP, 'partials'),
   });
   const html = getHtmlSource(result);
 
@@ -14,4 +16,27 @@ test('it allows for injecting a partial', async () => {
 
   // Inject a HBS file as a partial
   expect(html).toContain('<footer></footer>');
+});
+
+test('it handles no partial directory existing', async () => {
+  const result = await build(NO_PARTIAL_DIRECTORY_APP, {
+    partialDirectory: resolve(NO_PARTIAL_DIRECTORY_APP, 'partials'),
+  });
+  const html = getHtmlSource(result);
+
+  // Just make sure that we have _some_ HTML output
+  expect(html).toBeTruthy();
+});
+
+test('it handles the partial directory being empty', async () => {
+  // Create an empty "tmp" directory to be the partial directory
+  const partialDirectory = await mkdtemp('vite-plugin-handlebars--empty-partial-directory');
+
+  const result = await build(NO_PARTIAL_DIRECTORY_APP, {
+    partialDirectory,
+  });
+  const html = getHtmlSource(result);
+
+  // Just make sure that we have _some_ HTML output
+  expect(html).toBeTruthy();
 });
--- src/partials.ts
+++ src/partials.ts
@@ -1,4 +1,5 @@
 import { registerPartial } from 'handlebars';
+import { Dir } from 'fs';
 import { opendir, readFile } from 'fs/promises';
 import { resolve, parse } from 'path';
 
@@ -6,7 +7,15 @@ import { resolve, parse } from 'path';
  * Registers each HTML file in a directory as Handlebars partial
  */
 export async function registerPartials(directoryPath: string): Promise<void> {
-  const dir = await opendir(directoryPath);
+  let dir: Dir;
+
+  try {
+    dir = await opendir(directoryPath);
+  } catch (e) {
+    // No-op if the directory does not exist
+    return;
+  }
+
   for await (const entry of dir) {
     if (entry.isFile) {
       const fullPath = resolve(directoryPath, entry.name);
