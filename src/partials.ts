15c67b225543e99ac9553355a261dcbb269ffda6
Alex LaFroscia <alex@lafroscia.com>
2021-03-18 20:48:38 -0400

    fix: only register html or hbs files as partials

--- src/partials.ts
+++ src/partials.ts
@@ -2,6 +2,8 @@ import { registerPartial } from 'handlebars';
 import { opendir, readFile } from 'fs/promises';
 import { join, parse } from 'path';
 
+const VALID_EXTENSIONS = new Set(['.html', '.hbs']);
+
 // Borrowed from https://gist.github.com/lovasoa/8691344
 async function* walk(dir: string): AsyncGenerator<string> {
   for await (const d of await opendir(dir)) {
@@ -25,10 +27,13 @@ export async function registerPartials(directoryPath: string | Array<string>): P
     try {
       for await (const fileName of walk(path)) {
         const parsedPath = parse(fileName);
-        const partialName = join(parsedPath.dir, parsedPath.name).replace(`${path}/`, '');
-        const content = await readFile(fileName);
 
-        registerPartial(partialName, content.toString());
+        if (VALID_EXTENSIONS.has(parsedPath.ext)) {
+          const partialName = join(parsedPath.dir, parsedPath.name).replace(`${path}/`, '');
+          const content = await readFile(fileName);
+
+          registerPartial(partialName, content.toString());
+        }
       }
     } catch (e) {
       // This error indicates the partial directory doesn't exist; ignore it
