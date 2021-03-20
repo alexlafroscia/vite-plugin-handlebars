9e824525be5692c4fcea0c84624dc5ad57f586c7
Alex LaFroscia <alex@lafroscia.com>
2021-03-20 13:41:10 -0400

    chore: use `node14` base config for TypeScript

--- __tests__/helpers/rollup.ts
+++ __tests__/helpers/rollup.ts
@@ -5,14 +5,18 @@ type Output = OutputAsset | OutputChunk;
 
 export function getHtmlSource(rollupOutput: RollupOutput | RollupOutput[]): string {
   const output: Output[] = Array.isArray(rollupOutput)
-    ? rollupOutput.reduce((acc, output) => {
-        return [...acc, ...output.output];
-      }, [])
+    ? rollupOutput
+        .map((rollupOutput) => rollupOutput.output)
+        .reduce((acc, output) => [...acc, ...output], [] as Output[])
     : rollupOutput.output;
 
   // @ts-expect-error
   const outputAssets: OutputAsset[] = output.filter((file) => file.type === 'asset');
   const htmlOutput = outputAssets.find((file) => parse(file.fileName).ext === '.html');
+
+  if (!htmlOutput) {
+    throw new Error('No HTML output in bundle');
+  }
 
   return typeof htmlOutput.source === 'string' ? htmlOutput.source : htmlOutput.source.toString();
 }
