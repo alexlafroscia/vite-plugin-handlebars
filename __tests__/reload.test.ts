f2ce148cce854afb6c1de1329a38f18d2f55f2fd
Alex LaFroscia <alex@lafroscia.com>
2024-01-06 11:34:02 -0500

    disable `reload` tests in CI

--- __tests__/reload.test.ts
+++ __tests__/reload.test.ts
@@ -2,9 +2,6 @@ import { test, afterAll, expect, vi } from 'vitest';
 import { Factory as FixtureFactory } from 'file-fixture-factory';
 import { serve, waitFor, TimeoutError } from './helpers';
 
-const testWithoutWindows = (...args: Parameters<typeof test>) =>
-  process.platform === 'win32' ? test.skip(...args) : test(...args);
-
 const factory = new FixtureFactory('vite-plugin-handlebars', {
   root: __dirname,
 });
@@ -13,63 +10,69 @@ afterAll(async () => {
   await factory.disposeAll();
 });
 
-testWithoutWindows('it sends a `full-reload` event when an `hbs` partial changes', async () => {
-  const temp = await factory.createDirectory({
-    'index.html': '{{> foo }}',
-    partials: {
-      'foo.hbs': '<p>foo</p>',
-    },
-  });
-  const devServer = await serve(temp.dir, {
-    partialDirectory: temp.path('partials'),
-  });
+test.skipIf(process.env.DISABLE_RELOAD_TESTS)(
+  'it sends a `full-reload` event when an `hbs` partial changes',
+  async () => {
+    const temp = await factory.createDirectory({
+      'index.html': '{{> foo }}',
+      partials: {
+        'foo.hbs': '<p>foo</p>',
+      },
+    });
+    const devServer = await serve(temp.dir, {
+      partialDirectory: temp.path('partials'),
+    });
 
-  vi.spyOn(devServer.ws, 'send');
+    vi.spyOn(devServer.ws, 'send');
 
-  // Fake the user visiting the index page to build it
-  await devServer.transformIndexHtml('/', await temp.read('index.html'));
+    // Fake the user visiting the index page to build it
+    await devServer.transformIndexHtml('/', await temp.read('index.html'));
 
-  await temp.write('partials/foo.hbs', '<p>bar</p>');
+    await temp.write('partials/foo.hbs', '<p>bar</p>');
 
-  await waitFor(
-    () => {
-      expect(devServer.ws.send).toBeCalledWith({ type: 'full-reload' });
-    },
-    { timeout: 3000 },
-  );
+    await waitFor(
+      () => {
+        expect(devServer.ws.send).toBeCalledWith({ type: 'full-reload' });
+      },
+      { timeout: 3000 },
+    );
 
-  await devServer.close();
-});
+    await devServer.close();
+  },
+);
 
-testWithoutWindows('it sends a `full-reload` event when an `html` partial changes', async () => {
-  const temp = await factory.createDirectory({
-    'index.html': '{{> foo }}',
-    partials: {
-      'foo.html': '<p>foo</p>',
-    },
-  });
-  const devServer = await serve(temp.dir, {
-    partialDirectory: temp.path('partials'),
-  });
+test.skipIf(process.env.DISABLE_RELOAD_TESTS)(
+  'it sends a `full-reload` event when an `html` partial changes',
+  async () => {
+    const temp = await factory.createDirectory({
+      'index.html': '{{> foo }}',
+      partials: {
+        'foo.html': '<p>foo</p>',
+      },
+    });
+    const devServer = await serve(temp.dir, {
+      partialDirectory: temp.path('partials'),
+    });
 
-  vi.spyOn(devServer.ws, 'send');
+    vi.spyOn(devServer.ws, 'send');
 
-  // Fake the user visiting the index page to build it
-  await devServer.transformIndexHtml('/', await temp.read('index.html'));
+    // Fake the user visiting the index page to build it
+    await devServer.transformIndexHtml('/', await temp.read('index.html'));
 
-  await temp.write('partials/foo.html', '<p>bar</p>');
+    await temp.write('partials/foo.html', '<p>bar</p>');
 
-  await waitFor(
-    () => {
-      expect(devServer.ws.send).toBeCalledWith({ type: 'full-reload' });
-    },
-    { timeout: 3000 },
-  );
+    await waitFor(
+      () => {
+        expect(devServer.ws.send).toBeCalledWith({ type: 'full-reload' });
+      },
+      { timeout: 3000 },
+    );
 
-  await devServer.close();
-});
+    await devServer.close();
+  },
+);
 
-testWithoutWindows('reloading the browser can be disabled', async () => {
+test.skipIf(process.env.DISABLE_RELOAD_TESTS)('reloading the browser can be disabled', async () => {
   // Exact number of assertions between `waitFor` and the thrown TimeoutError
   expect.assertions(305);
 
